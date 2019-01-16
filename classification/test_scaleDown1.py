4ae2711ef3c5f7c9db65ee0a66628f1df2e8c887
Kate Weaver <kweave23@jhu.edu>
2019-01-16 14:43:09 -0500

    eval script

--- classification/test_scaleDown1.py
+++ classification/test_scaleDown1.py
@@ -18,16 +18,15 @@ import matplotlib.patches as mpatches
 '''Usage ./test_scaleDown1.py --SavedModelFile saved_model_ext --TestDataFile test_data'''
 
 parser = ap.ArgumentParser(description='Testing a PreTrained Model')
-parser.add_argument('--SavedModelFile', action='store', nargs='1', type=str, required = True, help='Filename for saved model')
-parser.add_argument('--TestDataFile', action='store', nargs='1', type=str, required = True, help="Filename for saved test data")
+parser.add_argument('--SavedModelFile', action='store', nargs=1, type=str, required = True, help='Filename for saved model')
+parser.add_argument('--TestDataFile', action='store', nargs=1, type=str, required = True, help="Filename for saved test data")
 args=parser.parse_args()
 
 model_ext = args.SavedModelFile
 test_data_file = args.TestDataFile
 
 ''' load saved estimator'''
-rsearch_best_estimator = joblib.load("/home-3/kweave23@jhu.edu/work/users/kweave23/out/{}".format(model_ext))
-params = rsearch_best_estimator.get_params()
+estimator = joblib.load(model_ext)
 
 ''' load test data'''
 with np.load("{}".format(test_data_file)) as npzfile:
@@ -35,27 +34,37 @@ with np.load("{}".format(test_data_file)) as npzfile:
     labels = npzfile['X_labels_test']
     merged_test_arrays = npzfile['merged_test_arrays']
 
-'''Function to produce metrics of loaded estimator from test data'''    
-def score(X, y, padding, alpha, filters, kernel_size, activation, pool_size, strides, epochs, needsCompiled, inputNodes, outputNodes, hiddenNodes, batchSize, steps_per_epoch, estimator):
+#default params for nn_wrap class
+params = {'alpha':0.3,
+		  'filters':20,
+		  'kernel_size':6,
+		  'pool_size': 4,
+		  'strides':2,
+		  'activation': 'elu',
+		  'epochs': 5,
+		  'batchSize': 32}
+
+'''Function to produce metrics of loaded estimator from test data'''
+def score(X, y, estimator, alpha, filters, kernel_size, pool_size, strides, activation, epochs, batchSize):
     batchSize = batchSize
     filters = filters
     kernel_size = kernel_size
     activation = activation
     pool_size = pool_size
     strides = strides
-    
+
     all_pred_y = []
     all_true_y = []
     instance_len = []
     total_instances = 0
-    
+
     print("Score print 1: ", X.shape)
-    
+
     sess = tf.InteractiveSession()
-    
+
     placeholder = tf.placeholder(np.int32, shape=X.shape)
     dataset = tf.data.Dataset.from_tensor_slices((placeholder, y)).batch(batchSize)
-    
+
     iterator = dataset.make_initializable_iterator()
     nextOutput = iterator.get_next()
     iterator.initializer.run(feed_dict = {placeholder:X})
@@ -79,50 +88,50 @@ def score(X, y, padding, alpha, filters, kernel_size, activation, pool_size, str
 
             init_g = tf.global_variables_initializer()
             init_g.run()
-            
-            convInput = tf.layers.conv1d(seq_tensor, 
-                                         filters, 
-                                         padding = 'same', 
+
+            convInput = tf.layers.conv1d(seq_tensor,
+                                         filters,
+                                         padding = 'same',
                                          activation= activation)
-        
-            convInput = tf.layers.max_pooling1d(convInput, 
-                                                pool_size, 
-                                                strides, 
+
+            convInput = tf.layers.max_pooling1d(convInput,
+                                                pool_size,
+                                                strides,
                                                 padding= 'same')
-                                                
+
             convInput2 = tf.layers.conv1d(convInput,
                                           filters,
                                           padding='same',
                                           activation = activation)
-                                        
+
             convInput2 = tf.layers.max_pooling1d(convInput2,
                                                  pool_size,
                                                  strides,
                                                  padding='same')
-            
+
             flattenSeq = tf.layers.Flatten()(convInput2)
 
             tf.initialize_all_variables().run()
             flattenSeqNA = flattenSeq.eval()
 
-            input_numpyArray2 = np.concatenate((flattenSeqNA, 
-                                               RNAseq_array, 
-                                               ATACseq_array), 
+            input_numpyArray2 = np.concatenate((flattenSeqNA,
+                                               RNAseq_array,
+                                               ATACseq_array),
                                               axis=1).astype(np.int32)
 
             print("Score print 3", input_numpyArray2.shape, "\n", labels_array.shape)
             pred_y = estimator.predict(input_numpyArray2)
-            
+
             total_instances += int(pred_y.shape[0])
             instance_len.append(int(pred_y.shape[0]))
             all_pred_y.append(pred_y)
             all_true_y.append(labels)
-            
+
             print("Score print 4: ", pred_y.shape)
-            
+
     except tf.errors.OutOfRangeError:
         pass
-    
+
     '''compute accuracy'''
     y_pred = []
     y_true = []
@@ -135,24 +144,24 @@ def score(X, y, padding, alpha, filters, kernel_size, activation, pool_size, str
             y_true.append(true)
             if predicted == true:
                 correct_pred += 1
-    
+
     num_classes = int(np.max(y_true)+1)
-    print("num_classes = 5: ", num_classes == 5)                
+    print("num_classes = 5: ", num_classes == 5)
     accuracy = correct_pred/total_instances
     print("score print 5: ", accuracy)
-    
+
     '''confusion_matrix'''
     confusion_matrix = skm.confusion_matrix(y_true, y_pred)
-                
-   
-            
+
+
+
     classification_report = skm.classification_report(y_true, y_pred)
-    
+
     '''binarize the predictions'''
     y_true = skp.label_binarize(y_true, classes=np.arange(5))
     y_pred = skp.label_binarize(y_pred, classes=np.arange(5))
 
-    
+
     '''precision, recall, average_precision, false_positive_rate, t_positive_rate, roc_auc'''
     # For each class
     precision = dict()
@@ -175,11 +184,11 @@ def score(X, y, padding, alpha, filters, kernel_size, activation, pool_size, str
                                                          average="micro")
     print('Average precision score, micro-averaged over all classes: {0:0.2f}'
           .format(average_precision["micro"]))
-    
+
     # Compute micro-average ROC curve and ROC area
     fpr["micro"], tpr["micro"], _ = skm.roc_curve(y_true.ravel(), y_pred.ravel())
     roc_auc["micro"] = skm.auc(fpr["micro"], tpr["micro"])
-    
+
     # Compute macro-average ROC curve and ROC area - First aggregate all false positive rates
     all_fpr = np.unique(np.concatenate([fpr[i] for i in range(num_classes)]))
 
@@ -194,29 +203,74 @@ def score(X, y, padding, alpha, filters, kernel_size, activation, pool_size, str
     fpr["macro"] = all_fpr
     tpr["macro"] = mean_tpr
     roc_auc["macro"] = skm.auc(fpr["macro"], tpr["macro"])
-              
+
     sess.close()
-    
-    return(accuracy, 
-           confusion_matrix, 
-           classification_report, 
+
+    return(accuracy,
+           confusion_matrix,
+           classification_report,
            precision,
            recall,
-           fpr
-           tpr
-           roc_auc
+           fpr,
+           tpr,
+           roc_auc,
            average_precision)
 
-'''Get metrics'''    
-accuracy, confusion_matrix, classification_report, precision, recall, fpr, tpr, roc_auc, average_precision = score(merged_test_arrays, labels, **params, rsearch_best_estimator)
+'''Get metrics'''
+accuracy, confusion_matrix, classification_report, precision, recall, fpr, tpr, roc_auc, average_precision = score(merged_test_arrays, labels, estimator, **params)
 
 num_classes = int(confusion_matrix.shape[0])
 print("num_classes = 5: ", num_classes == 5)
 
-print("Accuracy on test data: ", accuracy)        
-        
+print("Accuracy on test data: ", accuracy)
+
 '''Classification report with number labels'''
 print(classification_report)
+
+'''Confusion matrix with number labels'''
+def plot_confusion_matrix(cm, labeling=True, normalize=False, title='Confusion Matrix', cmap='Greys'):
+	if normalize:
+		cm = cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+		print("Normalized confusion matrix")
+
+	m = np.max(np.abs(cm))
+	vmin = 0 if normalize else -1*m
+	fig, ax = plt.subplots(figsize=(16,14))
+	ax.set_title(title, fontsize=20)
+	im = ax.pcolor(cm, cmap=cmap, vmin = vmin, vmax=m)
+	cbar = fig.colorbar(im, ax=ax)
+	tick_marks = np.arange(int(cm.shape[0]))
+	plt.xticks(tick_marks, tick_marks)
+	plt.yticks(tick_marks, tick_marks)
+
+	fmt = '.2f' if normalize else 'd'
+	thresh = m/2.0
+
+	for i,j in itertools.product(range(int(cm.shape[0])), range(int(cm.shape[1]))):
+		if cmap == 'Greys':
+			color ='white' if cm[i,j] > thresh else 'black'
+		elif cmap == 'viridis':
+			color = 'black' if cm[i,j] > thresh else 'white'
+		if labeling:
+			plt.text(j+0.5,i+0.5, format(cm[i,j],fmt), horizontalalignment='center', verticalalignment = 'center', color=color)
+
+	ax.set_ylabel('True label', fontsize=20)
+	ax.set_xlabel('Predicted label', fontsize=20)
+	plt.tight_layout()
+
+	fig.savefig("confusion_matrix_cmap_{}_normalize_{}_sqlabeling_{}_numberLabels.png".format(cmap, normalize, labeling))
+	plt.close(fig)
+
+plot_confusion_matrix(confusion_matrix, normalize=True, cmap='viridis')
+plot_confusion_matrix(confusion_matrix, normalize=True)
+plot_confusion_matrix(confusion_matrix)
+plot_confusion_matrix(confusion_matrix, labeling=False)
+plot_confusion_matrix(confusion_matrix, cmap='viridis')
+plot_confusion_matrix(confusion_matrix, cmap='viridis', labeling=False)
+plot_confusion_matrix(confusion_matrix, normalize=True, cmap='viridis', labeling=False)
+plot_confusion_matrix(confusion_matrix, normalize=True, labeling=False)
+print("8 confusion matrices plotted")
+print("-------End Metrics------")
 
 '''Plot the micro-averaged precision/recall curve'''
 fig,ax=plt.subplots(figsize=(15,15))
@@ -233,37 +287,10 @@ fig.suptitle(
     .format(average_precision["micro"]))
 fig.savefig("micro-averaged_precision_recall.png")
 plt.close(fig)
-    
+
 '''Plot the precision/recall curve for each class'''
-colors = itertools.cycle(['navy', #1
-                         'deeppink', #2
-                         'slategray', #3
-                         'darkorchid', #4
-                         'aqua',       #5
-                         'darkorange', #6
-                         'cornflowerblue', #7
-                         'darkkhaki', #8
-                         'olive', #9
-                         'darkgreen', #10 
-                         'fuchsia', #11
-                         'black', #12
-                         'darkslategray', #13 
-                         'darkviolet', #14
-                         'chocolate', #15
-                         'saddlebrown', #16
-                         'sandybrown', #17
-                         'mediumspringgreen', #18
-                         'darkgoldenrod', #19
-                         'chartreuse', #20
-                         'teal', #21
-                         'mediumblue', #22
-                         'brown', #23
-                         'indigo', #24
-                         'lightpink', #25
-                         'hotpink', #26
-                         'blue', #27
-                         'darkturquoise']) #28
-                         
+colors = itertools.cycle(['navy', 'deeppink', 'darkslategray', 'darkorchid', 'saddlebrown','darkgreen', 'darkturquoise'])
+
 fig,ax = plt.subplots(figsize=(20,15))
 lines = []
 labels = []
@@ -319,33 +346,7 @@ plt.plot(fpr["macro"], tpr["macro"],
                ''.format(roc_auc["macro"]),
          color='navy', linestyle=':', linewidth=4)
 
-colors = itertools.cycle(['slategray', #1
-                          'darkorchid', #2
-                          'aqua',       #3
-                          'darkorange', #4
-                          'cornflowerblue', #5
-                          'darkkhaki', #6
-                          'olive', #7
-                          'darkgreen', #8 
-                          'fuchsia', #9
-                          'black', #10
-                          'darkslategray', #11 
-                          'darkviolet', #12
-                          'chocolate', #13
-                          'saddlebrown', #14
-                          'gold', #15
-                          'sandybrown', #16
-                          'mediumspringgreen', #17
-                          'darkgoldenrod', #18
-                          'chartreuse', #19
-                          'teal', #20
-                          'mediumblue', #21
-                          'brown', #22
-                          'indigo', #23
-                          'lightpink', #24
-                          'hotpink', #25
-                          'blue', #26
-                          'darkturquoise'])#27
+colors = itertools.cycle(['darkslategray', 'darkorange', 'darkorchid', 'saddlebrown', 'darkturquoise', 'darkgreen'])
 for i, color in zip(range(num_classes), colors):
     plt.plot(fpr[i], tpr[i], color=color, lw=4,
              label='ROC curve of class {0} (area = {1:0.2f})'
@@ -362,54 +363,54 @@ plt.close(fig)
 
 
 
-'''Confusion matrix with number labels'''
-def plot_confusion_matrix2(cm,
-                          labeling=True,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap='Greys'):
-    if normalize:
-        cm =cm.astype('float')/cm.sum(axis=1)[:,np.newaxis] #np.newaxis is an alias for none
-        print("Normalized confusion matrix")
-    
-    m = np.max(np.abs(cm))
-    vmin = 0 if normalize else -1*m
-    fig, ax = plt.subplots(figsize=(16,14))
-    ax.set_title(title, fontsize=20)
-    im = ax.pcolor(cm, cmap=cmap, vmin = vmin, vmax=m)
-    cbar = fig.colorbar(im, ax=ax)
-    tick_marks = np.arange(int(cm.shape[0]))
-    #print(tick_marks)
-    plt.xticks(tick_marks, tick_marks)
-    plt.yticks(tick_marks, tick_marks)
-    
-    fmt = '.2f' if normalize else 'd'
-    thresh = m/2.0
-    
-    for i,j in itertools.product(range(int(cm.shape[0])), range(int(cm.shape[1]))):
-        if cmap == 'Greys':
-            color='white' if cm[i,j] > thresh else 'black'
-        elif cmap =='viridis':
-            color = 'black'if cm[i,j] > thresh else 'white'
-        if labeling:
-            plt.text(j+0.5,i+0.5, format(cm[i,j], fmt),
-                    horizontalalignment='center',
-                    verticalalignment = 'center',
-                    color=color)
-    ax.set_ylabel('True label', fontsize=20)
-    ax.set_xlabel('Predicted label', fontsize=20)
-    plt.tight_layout()
-    fig.savefig("confusion_matrix_cmap_{}_normalize_{}_sqlabeling{}_numberLabels.png".format(cmap, normalize, labeling))
-    plt.close(fig)
-
-plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', cmap='viridis')
-plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix')
-plot_confusion_matrix2(confusion_matrix, title='Confusion matrix')
-plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', labeling=False)
-plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', cmap='viridis')
-plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', cmap='viridis', labeling=False)
-plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', cmap='viridis', labeling=False)
-plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', labeling=False)
+# '''Confusion matrix with number labels'''
+# def plot_confusion_matrix2(cm,
+#                           labeling=True,
+#                           normalize=False,
+#                           title='Confusion matrix',
+#                           cmap='Greys'):
+#     if normalize:
+#         cm =cm.astype('float')/cm.sum(axis=1)[:,np.newaxis] #np.newaxis is an alias for none
+#         print("Normalized confusion matrix")
+#
+#     m = np.max(np.abs(cm))
+#     vmin = 0 if normalize else -1*m
+#     fig, ax = plt.subplots(figsize=(16,14))
+#     ax.set_title(title, fontsize=20)
+#     im = ax.pcolor(cm, cmap=cmap, vmin = vmin, vmax=m)
+#     cbar = fig.colorbar(im, ax=ax)
+#     tick_marks = np.arange(int(cm.shape[0]))
+#     #print(tick_marks)
+#     plt.xticks(tick_marks, tick_marks)
+#     plt.yticks(tick_marks, tick_marks)
+#
+#     fmt = '.2f' if normalize else 'd'
+#     thresh = m/2.0
+#
+#     for i,j in itertools.product(range(int(cm.shape[0])), range(int(cm.shape[1]))):
+#         if cmap == 'Greys':
+#             color='white' if cm[i,j] > thresh else 'black'
+#         elif cmap =='viridis':
+#             color = 'black'if cm[i,j] > thresh else 'white'
+#         if labeling:
+#             plt.text(j+0.5,i+0.5, format(cm[i,j], fmt),
+#                     horizontalalignment='center',
+#                     verticalalignment = 'center',
+#                     color=color)
+#     ax.set_ylabel('True label', fontsize=20)
+#     ax.set_xlabel('Predicted label', fontsize=20)
+#     plt.tight_layout()
+#     fig.savefig("confusion_matrix_cmap_{}_normalize_{}_sqlabeling{}_numberLabels.png".format(cmap, normalize, labeling))
+#     plt.close(fig)
+#
+# plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', cmap='viridis')
+# plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix')
+# plot_confusion_matrix2(confusion_matrix, title='Confusion matrix')
+# plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', labeling=False)
+# plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', cmap='viridis')
+# plot_confusion_matrix2(confusion_matrix, title='Confusion matrix', cmap='viridis', labeling=False)
+# plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', cmap='viridis', labeling=False)
+# plot_confusion_matrix2(confusion_matrix, normalize=True, title='Confusion matrix', labeling=False)
 
 # '''Confusion matrix with letter labels'''
 # def plot_confusion_matrix(cm, labels,
